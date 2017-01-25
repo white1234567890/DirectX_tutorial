3c6f7a22096638336686dd2512e5d1111b4eebf2
You Oyadomari <gene_crew_lunch@yahoo.co.jp>
2017-01-26 01:32:57 +0900

    Create input class

--- GameEngine/Input.cpp
+++ GameEngine/Input.cpp
@@ -1,11 +1,82 @@
 #include "Input.h"
 
-
+//////////////////////////////////////////////////////////////////////////////
+//Default constructor
+//////////////////////////////////////////////////////////////////////////////
 Input::Input(void)
 {
+	//Clear array that key is pressed state
+	for(size_t i = 0; i < inputNS::KEYS_ARRAY_LEN; i++)
+	{
+		keysDown[i] = false;
+	}
+
+	//Clear array that key is pressing state
+	for(size_t i = 0; i < inputNS::KEYS_ARRAY_LEN; i++)
+	{
+		keysPressed[i] = false;
+	}
+
+	newLine = true;	//Start new line
+	textIn = "";		//Clear textIn
+	charIn = 0;			//Clear charIn
+
+	//Mouse data
+	mouseX = 0;	//Window X axis
+	mouseY = 0;	//Window Y axis
+	mouseRawX = 0;	//High-definition X axis
+	mouseRawY = 0;	//High-definition Y axis
+	mouseLButton = false;	//If mouse left button is pressed, this variable is true
+	mouseMButton = false;	//If mouse middle button is pressed, this variable is true
+	mouseRButton = false;	//If mouse right button is pressed, this variable is true
+	mouseX1Button = false;	//If mouse X1 button is pressed, this variable is true
+	mouseX2Button = false;	//If mouse X2 button is pressed, this variabe is true
+
+	for(int i = 0; i < MAX_CONTROLLERS; i++)
+	{
+		controllers[i].vibrationTimeLeft = 0;
+		controllers[i].vibrationTimeRight = 0;
+	}
 }
 
-
+//////////////////////////////////////////////////////////////////////////////
+//Destructor
+//////////////////////////////////////////////////////////////////////////////
 Input::~Input(void)
 {
+	if(mouseCaptured) ReleaseCapture();	//Release mouse
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//Initialize mouse and controller input
+//If you want to capture mouse, capture = true
+//throw GameError
+//////////////////////////////////////////////////////////////////////////////
+void Input::initialize(HWND hwnd, bool capture)
+{
+	try
+	{
+		mouseCaptured = capture;
+
+		//Register high-definition mouse
+		Rid[0].usUsagePage = HID_USAGE_PAGE_GENERIC;
+		Rid[0].usUsage = HID_USAGE_GENERIC_MOUSE;
+		Rid[0].dwFlags = RIDEV_INPUTSINK;
+		Rid[0].hwndTarget = hwnd;
+
+		RegisterRawInputDevices(Rid, 1, sizeof(Rid[0]));
+
+		if(mouseCaptured)
+		{
+			SetCapture(hwnd);	//Capture mouse
+		}
+
+		//Clear controller
+		ZeroMemory(controllers, sizeof(ControllerState) * MAX_CONTROLLERS);
+		checkControllers();	//Check connected controller
+	}
+	catch(...)
+	{
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error initialize input system"));
+	}
 }
--- GameEngine/Input.h
+++ GameEngine/Input.h
@@ -6,6 +6,11 @@
 #pragma once
 #include <Windows.h>
 #include <XInput.h>
+#include "GameError.h"
+
+
+#define HID_USAGE_PAGE_GENERIC ((USHORT) 0x01)
+#define HID_USAGE_GENERIC_MOUSE ((USHORT) 0x02)
 
 namespace inputNS
 {
@@ -21,31 +26,40 @@ typedef struct ControllerState
 	float vibrationTimeLeft;	//Miri second
 	float vibrationTimeRight;	//Miri second
 	bool connected;
-};
+}ControllerState;
 
 class Input
 {
 private:  
-	bool keysDown[inputNS::KEYS_ARRAY_LEN];			//If specified keys are pressed, this variable is true
-	bool keysPressed[inputNS::KEYS_ARRAY_LEN];		//When specified keys are pressing, this variable is true
-	std::string textIn;								//Text inputed by user
-	char charIn;									//Last inputed charactor
-	bool newLine;									//When start new line, this variable is true
-	int mouseX,mouseY;								//Mouse location in window
-	int mouseRawX,mouseRawY;						//High-definition mouse location in window
-	RAWINPUTDEVICE Rid[1];							//For High definition mouse
-	bool mouseCaptured;								//If mouse was captured, this variable is true
-	bool mouseLButton;								//If mouse left button is pressed, this variable is true
-	bool mouseMButton;								//If mouse middle button is pressed, this variable is true
-	bool mouseRButton;								//If mouse right button is pressed, this variable is true
-	bool mouseX1Button;								//If mouse X1 button is pressed, this variable is true
-	bool mouseX2Button;								//If mouse X2 button is pressed, this vaiable is true
+	bool keysDown[inputNS::KEYS_ARRAY_LEN];				//If specified keys are pressed, this variable is true
+	bool keysPressed[inputNS::KEYS_ARRAY_LEN];			//When specified keys are pressing, this variable is true
+	std::string textIn;												//Text inputed by user
+	char charIn;														//Last inputed charactor
+	bool newLine;														//When start new line, this variable is true
+	int mouseX,mouseY;												//Mouse location in window
+	int mouseRawX,mouseRawY;									//High-definition mouse location in window
+	RAWINPUTDEVICE Rid[1];										//For High definition mouse
+	bool mouseCaptured;											//If mouse was captured, this variable is true
+	bool mouseLButton;												//If mouse left button is pressed, this variable is true
+	bool mouseMButton;												//If mouse middle button is pressed, this variable is true
+	bool mouseRButton;												//If mouse right button is pressed, this variable is true
+	bool mouseX1Button;											//If mouse X1 button is pressed, this variable is true
+	bool mouseX2Button;											//If mouse X2 button is pressed, this vaiable is true
 	ControllerState controllers[MAX_CONTROLLERS];	//Contrpller state	
 
 public:
 	Input(void);
 	~Input(void);
 
+	//Set array of RAWINPUTDEVICE Rid
+	//Resolution of standard conputer mouse is 400 dpi(dot per inch), but high-definition mouse is more than 800 dpi
+	//High-definition mouse can not get from default WM_MOUSEMOVE message
+	//If necessary capture mouse, clear array stored controller state
+	//If mouse is captured, mouse input send to capture window
+	//This is necessary game that player move is controled by mouse
+	//Standard mouse bihavior do not need mouse capture
+	void initialize(HWND hwnd, bool capture);
 
+	void checkController();
 };
 
